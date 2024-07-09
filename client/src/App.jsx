da8cda65688142981c8b70d80f9706049c771f55
AidanGio <agiordan@stevens.edu>
2024-07-09 18:05:38 -0400

    vite url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 
-const apiUrl = import.meta.env.REACT_APP_API_URL || "http://localhost:3000";
+const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 const App = () => {
     const [selectedFile, setSelectedFile] = useState(null);
