24c34f51fe485f238a3f7e08527245ca214ad20a
AidanGio <agiordan@stevens.edu>
2024-07-09 18:33:25 -0400

    cors

--- server/server.js
+++ server/server.js
@@ -10,21 +10,7 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = ["https://10q-analysis-client.vercel.app", "http://localhost:5173"];
-
-app.use(
-    cors({
-        origin: function (origin, callback) {
-            if (!origin) return callback(null, true);
-            if (allowedOrigins.indexOf(origin) === -1) {
-                var msg = "The CORS policy for this site does not allow access from the specified Origin.";
-                return callback(new Error(msg), false);
-            }
-            return callback(null, true);
-        },
-        credentials: true,
-    })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
